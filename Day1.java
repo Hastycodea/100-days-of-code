41b9788de468180eb76e7ec3ad758cae25febf70
unknown <hastycodeandreh@gmail.com>
2024-06-21 11:10:38 +0300

    Hello World

--- Day1.java
+++ Day1.java
@@ -1,5 +1,5 @@
 public class Day1 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!"); //prints Hello World!
     }
 }
