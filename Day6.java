8859c14925446c0ab87d98b1a3251a5042a5c4a1
unknown <hastycodeandreh@gmail.com>
2024-06-25 11:29:11 +0300

    Type Conversion

---
+++ Day6.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Day6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        String number = scanner.nextLine();
+
+        int numberInteger = Integer.valueOf(number);
+        System.out.println("Your number as Integer: " + numberInteger);
+
+        double numberDouble = Double.valueOf(number);
+        System.out.println("Your number as Double value: " + numberDouble);
+
+        int age = 5;
+        // Integer to a string
+        String ageString = String.valueOf(age);
+        System.out.println("int age in String using valueOf(): " + ageString);
+
+        int marks = 80;
+        String marksToString = Integer.toString(marks);
+        System.out.println("int mark to String using toString(): " + marksToString);
+
+    }
+}
