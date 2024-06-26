578cd26e181efce86f4cbf2817d851a5ad4e7c1b
unknown <hastycodeandreh@gmail.com>
2024-06-26 11:40:38 +0300

    Math Operations

---
+++ Day7.java
@@ -0,0 +1,24 @@
+public class Day7 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 8;
+        double c = -5.27;
+
+        //Maximum of 2 numbers
+        System.out.println("Max: " + Math.max(a, b));
+
+        // Miniimum of 2 numbers
+        System.out.println("Min: " + Math.min(a, b));
+
+        // Square root of a number
+        System.out.println("Sqrt: " + Math.sqrt(a));
+
+        // Floor and Ceil of a float
+        System.out.println("Floor: " + Math.floor(c));
+        System.out.println("Ceil: " + Math.ceil(c));
+
+        // Abs value of a double
+        System.out.println("Abs: " + Math.abs(c));
+
+    }
+}
