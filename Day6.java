e5296c367568b8908868cd4f324bed4164301e29
unknown <hastycodeandreh@gmail.com>
2024-06-26 11:41:51 +0300

    Type Conversion

--- Day6.java
+++ Day6.java
@@ -14,10 +14,11 @@ public class Day6 {
         System.out.println("Your number as Double value: " + numberDouble);
 
         int age = 5;
-        // Integer to a string
+        // Integer to a string using valueOf()
         String ageString = String.valueOf(age);
         System.out.println("int age in String using valueOf(): " + ageString);
 
+        // Integer to a string using toString()
         int marks = 80;
         String marksToString = Integer.toString(marks);
         System.out.println("int mark to String using toString(): " + marksToString);
