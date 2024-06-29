5eda7eba27fba27924b0dacb0192c5a21fb5d0d3
unknown <hastycodeandreh@gmail.com>
2024-06-29 08:19:33 +0300

    Loops (for, while, do-while)

--- Day10.java
+++ Day10.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Day10 {
@@ -6,9 +7,11 @@ public class Day10 {
         System.out.print("Enter an integer: ");
         int number = Integer.valueOf(scanner.nextLine());
 
-        // forLoop(number);
-        // whileLoop(number);
+        forLoop(number);
+        whileLoop(number);
         doWhileLoop(number);
+        sumOfEven(number);
+        randomDivisibleBy5And7();
 
     }
 
@@ -40,5 +43,27 @@ public class Day10 {
         } while (i <= number);
         System.out.println("Do while loop sum: " + sum);
 
+    }
+
+    public static void sumOfEven(int number) {
+        int sum = 0;
+        for (int i = 1; i <= number; i++) {
+            if (i % 2 == 0) {
+                sum += i;
+            }
+        }
+        System.out.println("Sum of Even: " + sum);
+    }
+
+    public static void randomDivisibleBy5And7() {
+        Random rand = new Random();
+
+        while (true) {
+            int randNumber = rand.nextInt(101);
+            if (randNumber % 5 == 0 && randNumber % 7 == 0) {
+                System.out.println("The number is: " + randNumber);
+            }
+            break;
+        }
     }
 }
