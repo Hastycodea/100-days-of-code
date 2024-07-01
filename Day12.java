83d7191ee663092fd3174f576e6b8cdc67e077b8
unknown <hastycodeandreh@gmail.com>
2024-07-01 12:10:35 +0300

    StringBuffer and StringBuilder

--- Day12.java
+++ Day12.java
@@ -4,11 +4,13 @@ public class Day12 {
         String space = " ";
         String world = "World";
 
+        StringBuffer valueReverse = value.reverse();
+        System.out.println(valueReverse);
+
         StringBuilder value2 = new StringBuilder("Hello");
         
         System.out.println(value.append(space).append(world));
         System.out.println(value2.append(space).append(world));
-
         
     }
 }
