84d324538c9fa024280276f0b5f833340ce7c00b
unknown <hastycodeandreh@gmail.com>
2024-07-02 22:15:02 +0300

    StringBuffer and StringBuilder

--- Day12.java
+++ Day12.java
@@ -4,13 +4,15 @@ public class Day12 {
         //  StringBuilder: Use when you need to modify characters frequently in a single-threaded environment.
         //  StringBuffer: Use when you need to modify characters frequently in a multi-threaded environment.
 
-        StringBuffer value = new StringBuffer("Hello");
+        StringBuffer value1 = new StringBuffer("Hello");
         String space = " ";
         String world = "World";
 
         StringBuilder value2 = new StringBuilder("Hello");
+        StringBuilder value3 = new StringBuilder("Hello");
+        StringBuilder value4 = new StringBuilder("Hello");
         
-        System.out.println("StringBuffer result: " + value.append(space).append(world));
+        System.out.println("StringBuffer result: " + value1.append(space).append(world));
         System.out.println("StringBuilder result: " + value2.append(space).append(world));
         
     }
