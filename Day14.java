7b8e75e729699f8ac2b02149fae83047c9e31123
unknown <hastycodeandreh@gmail.com>
2024-07-03 15:33:12 +0300

    Updated 2D Array

--- Day14.java
+++ Day14.java
@@ -11,13 +11,28 @@ public class Day14 {
         System.out.println(Arrays.toString(maxEachRow(arrayValues)));
         System.out.println(Arrays.toString(maxEachColumn(arrayValues)));
 
+        for (int i = 0; i < arrayValues.length; i++) {
+            for (int j = 0; j < arrayValues[i].length; j++) {
+                System.out.print(arrayValues[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        int[][] transformedArray = transformArray(arrayValues);
+        for (int i = 0; i < transformedArray.length; i++) {
+            for (int j = 0; j < transformedArray[i].length; j++) {
+                System.out.print(transformedArray[i][j] + " ");
+            }
+            System.out.println();
+        }
+
     }
 
     public static int[] maxEachRow(int[][] arrayTwo) {
         int[] numbers = new int[arrayTwo.length];
-        for (int i = 0; i < arrayTwo.length; i++) {
-            int max = arrayTwo[i][0];
-            for (int j = 0; j < arrayTwo[i].length; j++) {
+        for (int i = 0; i < arrayTwo.length; i++) { // iterating over the rows
+            int max = arrayTwo[i][0]; // initializing max with 1st element of the current row
+            for (int j = 0; j < arrayTwo[i].length; j++) { // iterating of over the colums of the current row
                 if (arrayTwo[i][j] > max) {
                     max = arrayTwo[i][j];
                 }
@@ -30,17 +45,29 @@ public class Day14 {
     }
 
     public static int[] maxEachColumn(int[][] arrayMaxCol) {
-        int[] numbers = new int[arrayMaxCol[0].length]; 
+        int[] numbers = new int[arrayMaxCol[0].length];
 
-        for(int i = 0; i < arrayMaxCol[0].length; i++) { // iterating over the colums
+        for (int i = 0; i < arrayMaxCol[0].length; i++) { // iterating over the columns
             int max = arrayMaxCol[0][i];
-            for(int j = 0; j < arrayMaxCol.length; j++) { // iterating over the rows
+            for (int j = 0; j < arrayMaxCol.length; j++) { // iterating over the rows
                 if (arrayMaxCol[j][i] > max) {
                     max = arrayMaxCol[j][i];
                 }
             }
             numbers[i] = max;
         }
-        return numbers; 
+        return numbers;
+    }
+
+    public static int[][] transformArray(int[][] firstArray) {
+        int[][] secondArray = new int[firstArray[0].length][firstArray.length];
+
+        for (int i = 0; i < firstArray.length; i++) { // iterating over rows
+            for (int j = 0; j < firstArray[0].length; j++) {
+                int value = firstArray[i][j];
+                secondArray[j][i] = value;
+            }
+        }
+        return secondArray;
     }
 }
