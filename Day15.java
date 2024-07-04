0fbfe3458c29076451a9dd3ccda86a563c1c6b91
unknown <hastycodeandreh@gmail.com>
2024-07-04 21:42:00 +0300

    Updated ArrayList

--- Day15.java
+++ Day15.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Day15 {
@@ -18,32 +19,38 @@ public class Day15 {
         weekdays.stream().forEach(day -> System.out.println(day));
         System.out.println();
 
-        // Sorting elements alphabetically
-        System.out.println("Sorting elements alphabetically");
-        weekdays.stream().sorted().forEach(day -> System.out.println(day));
-        System.out.println();
-
-        // Sorting elements in decreasing order alphabetically
-        System.out.println("Sorting elements in decreasing order alphabetically");
-        Collections.sort(weekdays, (String d1, String d2) -> {
-                return d2.compareTo(d1);
-        });
+        // Removing days starting with char "character"
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a character to remove from the list: ");
+        char input = scanner.nextLine().charAt(0);
+        System.out.println("Here's the new list");
+        removesChar(weekdays, input);
         weekdays.stream().forEach(day -> System.out.println(day));
 
-        final Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a character to remove from the list: ");
-        String input = scanner.nextLine();
-        System.out.println("Here's the new list");
-        System.out.println(removesChar(weekdays, input).size());
-        // removedChar.stream().forEach(day -> System.out.println(day));
+        // // Sorting elements alphabetically
+        // System.out.println("Sorting elements alphabetically");
+        // weekdays.stream().sorted().forEach(day -> System.out.println(day));
+        // System.out.println();
+
+        // // Sorting elements in decreasing order alphabetically
+        // System.out.println("Sorting elements in decreasing order alphabetically");
+        // Collections.sort(weekdays, (String d1, String d2) -> {
+        //     return d2.compareTo(d1);
+        // });
+        // weekdays.stream().forEach(day -> System.out.println(day));
+
     }
 
-    public static ArrayList<String> removesChar(ArrayList<String> array, String character) {
-        for(String element : array) {
-            if (element.contains(character)) {
-                array.remove(element);                
+    public static ArrayList<String> removesChar(ArrayList<String> array, char myChar) {
+        Iterator<String> iterator = array.iterator();
+        while (iterator.hasNext()) {
+            char elementChar = iterator.next().charAt(0);
+            if (elementChar == myChar) {
+                iterator.remove();
             }
         }
+
         return array;
+
     }
 }
