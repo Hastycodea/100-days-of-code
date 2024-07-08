bc55ef5de938f1631841f616870eab27c85cdd59
Hastycode Andreh <87606630+Hastycodea@users.noreply.github.com>
2024-07-08 12:16:30 +0300

    Random Number Generation

---
+++ Day19.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+
+public class Day19 {
+    public static void main(String[] args) {
+
+        System.out.println("The generated random number: " +randomNumber());
+        System.out.println("Random between to ints: " + randomTwoInt(1, 7));
+    }
+
+    public static int randomNumber() {
+        Random rand = new Random();
+        return rand.nextInt();
+    }
+
+    public static int randomTwoInt(int a, int b) {
+        Random rand = new Random();
+        return rand.nextInt(a) + b;
+    }
+}
