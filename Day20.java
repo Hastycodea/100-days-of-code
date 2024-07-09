0e4719e7e4da74c36f425e24fd0dc9c3ef468c8e
Hastycode Andreh <87606630+Hastycodea@users.noreply.github.com>
2024-07-09 11:42:24 +0300

    Updated Set/HashSet

--- Day20.java
+++ Day20.java
@@ -1,19 +1,26 @@
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Day20 {
     public static void main(String[] args) {
+        Set<String> treeSet = new TreeSet<>();
+        Collections.addAll(treeSet, "juxta", "zodiac", "aaty");
+        System.out.println(treeSet.toString());
+
+
         Set<String> hashSet = new HashSet<>();
-        Collections.addAll(hashSet, "andreh", "brian", "kennedy", "junior");
+        Collections.addAll(hashSet, "andreh", "brian", "junior", "kennedy", "junior");
+        
+        // current list
+        System.out.println(hashSet.toString());
+        
         boolean result = hashSet.contains("bian");
         System.out.println(result);
 
-        // current list
-        System.out.println(hashSet.toString());
-
-        hashSet.remove("andreh");
         // deleted item list
+        hashSet.remove("andreh");
         System.out.println(hashSet.toString());
 
         // number of values(size) in the Set
