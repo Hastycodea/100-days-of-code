44bc2213a5716ad5e8645fb12a44fb037d283ad7
JujuJavaJuju <andrehandere254@gmail.com>
2024-07-11 12:12:17 +0300

    Stack

---
+++ Day22.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+
+public class Day22 {
+    public static void main(String[] args) {
+        Stack<String> values = new Stack<>();
+        values.add("Juju");
+        values.add("Kankan");
+        values.add("Juma");
+
+        System.out.println(values);
+
+        //removing the top element from the Stack
+        System.out.println(values.pop());
+        System.out.println(values);
+
+        // printing top element of the Stack without removing from it
+        System.out.println(values.peek());
+        System.out.println(values);
+
+        // checking if a value exists in the Stack
+        System.out.println(values.contains("Juju"));
+
+        // printing size in the Stack
+        System.out.println(values.size());
+
+        // removing all the values from the Stack
+        values.removeAllElements();
+        System.out.println(values.size());
+    }
+}
