e7f5ecb93f232a63a14231d06bf06bf4756789e7
hastycodea <hastycodeandreh@gmail.com>
2024-07-16 11:27:46 +0300

    Inheritance

---
+++ Day27/Instructor.java
@@ -0,0 +1,39 @@
+package Day27;
+
+public class Instructor extends User {
+    private String qualification;
+    private String coursesTaught;
+    private int salary;
+
+    public Instructor(String firstName, String lastName, String qualification, String coursesTaught, int salary) {
+        super(firstName, lastName);
+        this.qualification = qualification;
+        this.coursesTaught = coursesTaught;
+        this.salary = salary;
+    }
+
+    public String getQualification() {
+        return qualification;
+    }
+
+    public String getCoursesTaught() {
+        return coursesTaught;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Instructor [qualification=" + qualification + ", coursesTaught=" + coursesTaught + ", salary=" + salary
+                + ", getFirstName()=" + getFirstName() + ", getLastName()=" + getLastName() + "]";
+    }
+
+    public static void main(String[] args) {
+        Instructor john = new Instructor("john", "omwaka", "doctor", "Computer", 4000);
+        System.out.println(john.toString());
+        // System.out.println(john.getEmail());
+    }
+
+}
