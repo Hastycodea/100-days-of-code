8934cf8e484fa33214427afb01293cd565c470d0
hastycodea <hastycodeandreh@gmail.com>
2024-07-17 11:33:45 +0300

    Method Overloading

---
+++ Day28/User.java
@@ -0,0 +1,74 @@
+package Day27;
+
+public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String city;
+    private String country;
+
+    public User(int id, String firstName, String lastName, String email, String city, String country) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.city = city;
+        this.country = country;
+    }
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public User(String email) {
+        this.email = email;
+    }
+
+    public User(String firstName, String lastName, String email) {
+        this(firstName, lastName);
+        this.email = email;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public String getCountry() {
+        return country;
+    }
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    @Override
+    public String toString() {
+        return "Day25 [getFirstName()=" + getFirstName() + ", getLastName()=" + getLastName() + "]";
+    }
+
+}
