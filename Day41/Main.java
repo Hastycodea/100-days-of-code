15f9ca1f7be6d691affd711812f2876bc23f836f
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-07-30 12:14:49 +0300

    Regex email

---
+++ Day41/Main.java
@@ -0,0 +1,22 @@
+package Day41;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Enter a string: ");
+            String input = scanner.nextLine();
+
+            String email = "([(a-z) || (A-Z)]+||[0-9]+||([0-9]+)([a-z]+))(@)[a-z]+(.)[a-z]+";
+
+            if (input.matches(email)) {
+                System.out.println("The entered email is corect");
+            } else {
+                System.out.println("The email entered is incorrect");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
