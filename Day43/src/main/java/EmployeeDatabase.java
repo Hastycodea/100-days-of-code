3fad3960469e79e610f55ebeeb1e5d069e16370d
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-08-04 13:26:14 +0300

    CRUD - updated delete and read

--- Day43/src/main/java/EmployeeDatabase.java
+++ Day43/src/main/java/EmployeeDatabase.java
@@ -7,7 +7,6 @@ public class EmployeeDatabase {
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws Exception {
-        EmployeeDatabase employeeDatabase = new EmployeeDatabase();
 
         String url = "jdbc:mysql://127.0.0.1:3306/myDB";
         String user = "root";
@@ -15,20 +14,73 @@ public class EmployeeDatabase {
 
         conn = DriverManager.getConnection(url, user, password);
 
-        System.out.println("1. Insert Employee");
-        System.out.println("2. Update Employee");
-        System.out.print("Enter a choice: ");
-        int choice = Integer.parseInt(scanner.nextLine());
 
-        switch (choice) {
+        EmployeeDatabase employeeDatabase = new EmployeeDatabase();
+//        int value = 0;
+
+        while(true){
+            System.out.println();
+            System.out.println("1. Insert Employee");
+            System.out.println("2. Update Employee");
+            System.out.println("3. View Employees");
+            System.out.println("4. Delete Employee");
+            System.out.println("5. Exit");
+            System.out.print("Enter a choice: ");
+
+            int choice = Integer.parseInt(scanner.nextLine());
+            switch (choice) {
                 case 1:
                     employeeDatabase.insertMethod();
                     break;
                 case 2:
                     employeeDatabase.updateMethod();
+                    break;
+                case 3:
+                    employeeDatabase.viewEmployees();
+                    break;
+                case 4:
+                    employeeDatabase.deleteMethod();
+                    break;
+                case 5:
+                    System.exit(0);
+//                    value = 1;
                 default:
                     break;
+            }
         }
+    }
+
+    private void viewEmployees() throws SQLException {
+        String sql = "SELECT * FROM employees";
+        Statement statement = conn.createStatement();
+
+        ResultSet resultSet = statement.executeQuery(sql);
+        while (resultSet.next()) {
+            String city = resultSet.getString("city");
+            String first_name = resultSet.getString("first_name");
+            String last_name = resultSet.getString("last_name");
+            int pay = resultSet.getInt("pay");
+            int days = resultSet.getInt("days");
+            String email = resultSet.getString("email");
+
+            System.out.println("city: " + city + " first_name: " + first_name + " last_name: " + last_name + " pay: " + pay + " days: " + days + " email: " + email);
+        }
+    }
+
+    private void deleteMethod() throws SQLException {
+        System.out.print("Enter the name of the employee to be deleted: ");
+        String name = scanner.nextLine();
+        String sql = "DELETE FROM employees where first_name = ?";
+        PreparedStatement preparedStatement = conn.prepareStatement(sql);
+        preparedStatement.setString(1, name);
+        preparedStatement.execute();
+
+        if (preparedStatement.execute()) {
+            System.out.println("Employee deleted successfully");
+        } else {
+            System.out.println("Employee does not exist");
+        }
+
     }
 
     private void insertMethod() throws SQLException {
