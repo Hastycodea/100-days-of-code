6135a199705cd558974b144dfca65a6c44d4c85c
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-08-05 23:18:51 +0300

    Spring Initializer

---
+++ Day47/DemoApp/src/test/java/com/hastycode/DemoApp/DemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hastycode.DemoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
