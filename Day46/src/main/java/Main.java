e62e5e69b219f357d77d9f1dfb2e245c07f7612a
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-08-05 15:04:46 +0300

    JDBC: Advanced Concepts Batch Processing

--- Day46/src/main/java/Main.java
+++ Day46/src/main/java/Main.java
@@ -1,5 +1,39 @@
-public class Main {
-    public static void main(String[] args) {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+public class Main {
+    public static void main(String[] args) throws SQLException {
+
+        String url = "jdbc:mysql://127.0.0.1:3306/myDB";
+        String usr = "root";
+        String psswd = "sir2kalFOUR!";
+        Connection connection = DriverManager.getConnection(url, usr, psswd);
+
+        connection.setAutoCommit(false);
+
+        Statement statement = connection.createStatement();
+
+        String sql1 = "insert into students (first_name, second_name, marks, gender) values('Tim', 'Tsv', 332, 'male')";
+        String sql2 = "insert into students (first_name, second_name, marks, gender) values('Vim', 'Kimani', 332, 'male')";
+        String sql3 = "insert into students (first_name, second_name, marks, gender) values('Pim', 'Tsv', 332, 'male')";
+
+        statement.addBatch(sql1);
+        statement.addBatch(sql2);
+        statement.addBatch(sql3);
+        int[] rows = statement.executeBatch();
+
+        for (int i : rows) {
+            if (i > 0) {
+                continue;
+            } else {
+                connection.rollback();
+                System.out.println("An error occurred");
+                break;
+            }
+        }
+        connection.commit();
+        System.out.println("No errors were thrown");
     }
 }
