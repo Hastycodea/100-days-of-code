3ba0f9b3437743031a2a8d27c1ef6eb074c19198
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-08-06 11:30:17 +0300

    dependency injection

---
+++ Day47/Program/src/test/java/com/hastycode/Program/ProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hastycode.Program;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
