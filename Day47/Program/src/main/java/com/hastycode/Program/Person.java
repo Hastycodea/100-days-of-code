1b12ba3f37940a7d4bb31b3ccb84df7c2e11dbd1
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-08-06 11:52:10 +0300

    updated DI

---
+++ Day47/Program/src/main/java/com/hastycode/Program/Person.java
@@ -0,0 +1,10 @@
+package com.hastycode.Program;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+    public void build() {
+        System.out.println("Building a person");
+    }
+}
--- Day47/Program/src/main/java/com/hastycode/Program/ProgramApplication.java
+++ Day47/Program/src/main/java/com/hastycode/Program/ProgramApplication.java
@@ -2,12 +2,18 @@ package com.hastycode.Program;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class ProgramApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProgramApplication.class, args);
+
+		ApplicationContext context = SpringApplication.run(ProgramApplication.class, args);
+
+		Person obj = context.getBean(Person.class);
+		obj.build();
+
 	}
 
 }
