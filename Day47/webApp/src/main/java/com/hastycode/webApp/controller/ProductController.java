05065fc5ac026ac70cba3d46f4e6397640bc0fd5
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-08-08 08:17:03 +0300

    GetPostPutDelete

--- Day47/webApp/src/main/java/com/hastycode/webApp/controller/ProductController.java
+++ Day47/webApp/src/main/java/com/hastycode/webApp/controller/ProductController.java
@@ -3,20 +3,38 @@ package com.hastycode.webApp.controller;
 import com.hastycode.webApp.model.Product;
 import com.hastycode.webApp.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class ProductController {
 
-
     @Autowired
     ProductService productService;
 
-    @RequestMapping("/products")
+    @GetMapping("/products")
     public List<Product> getProducts() {
         return productService.getProducts();
+    }
+
+    @GetMapping("/products/{productId}")
+    public Product getProductById(@PathVariable int productId) {
+        return productService.getProdById(productId);
+    }
+
+    @PostMapping("/products")
+    public void addProduct(@RequestBody Product product) {
+        productService.addProduct(product);
+    }
+
+    @PutMapping("/products")
+    public void updateProduct(@RequestBody Product product) {
+        productService.updateProduct(product);
+    }
+
+    @DeleteMapping("/products/{productId}")
+    public void deleteProduct(@PathVariable int productId) {
+        productService.deleteProduct(productId);
     }
 }
--- Day47/webApp/src/main/java/com/hastycode/webApp/service/ProductService.java
+++ Day47/webApp/src/main/java/com/hastycode/webApp/service/ProductService.java
@@ -3,20 +3,45 @@ package com.hastycode.webApp.service;
 import com.hastycode.webApp.model.Product;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class ProductService {
 
-    List<Product> products = Arrays.asList(
+    List<Product> products = new ArrayList<>( Arrays.asList(
             new Product(14, "Tissue", 89),
             new Product(56, "Mirror", 78),
             new Product(88, "Exercise Book", 10)
-    );
+    ));
 
     public List<Product> getProducts() {
         return products;
     }
 
+    public Product getProdById(int productId) {
+        return products.stream()
+                .filter(p -> p.getProductId() == productId)
+                .findFirst()
+                .orElse(new Product(22, "null", 0));
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public void updateProduct(Product product) {
+        int index = 0;
+        for(int i=0; i < products.size(); i++) {
+            if (products.get(i).getProductId() == product.getProductId())
+                index = i;
+        }
+        products.set(index, product);
+    }
+
+    public void deleteProduct(int productId) {
+        Product product = products.stream().filter(p -> p.getProductId() == productId).findFirst().get();
+        products.remove(product);
+    }
 }
